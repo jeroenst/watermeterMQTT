c3ca64da90776e322f5e7bdbb5ead2fc1345c6b2
Jeroen <jst@dse.nl>
2018-03-06 08:11:47 +0100

    Added support for systemd

---
+++ watermetermysqlMQTT.php
@@ -0,0 +1,129 @@
+#!/usr/bin/php
+<?php
+
+require(realpath(dirname(__FILE__))."/../phpMQTT/phpMQTT.php");
+
+
+$server = "127.0.0.1";     // change if necessary
+$port = 1883;                     // change if necessary
+$username = "";                   // set your username
+$password = "";                   // set your password
+$client_id = "watermetermysqlMQTT"; // make sure this is unique for connecting to sever - you could use uniqid()
+$topicprefix = 'home/watermeter/';
+
+$settings = array(
+"mysqlserver" => "localhost",
+"mysqlusername" => "casaan",
+"mysqlpassword" => "gWdtGxQDnq6NhSeG",
+"mysqldatabase" => "casaan");
+
+
+$mqttdata = array();
+
+$mqtt = new phpMQTT($server, $port, $client_id);
+
+$lastgasdatetime = "";
+
+if(!$mqtt->connect(true, NULL, $username, $password)) {
+	exit(1);
+}
+
+echo "Connected to mqtt server...\n";
+$topics[$topicprefix.'m3'] = array("qos" => 0, "function" => "newvalue");
+$mqtt->subscribe($topics, 0);
+
+while($mqtt->proc()){
+}
+
+
+$mqtt->close();
+
+function newvalue($topic, $msg)
+{
+	echo "$topic = $msg\n";
+	static $mqttdata;
+	global $mqtt;
+	global $topicprefix;
+	global $settings;
+	static $lastgasdatetime;
+	
+       $newdata["total"]["m3"] = $msg;
+	
+		echo ("Calculating new water values...\n"); 
+		
+	        $mysqli = mysqli_connect($settings["mysqlserver"],$settings["mysqlusername"],$settings["mysqlpassword"],$settings["mysqldatabase"]);
+
+	        if (($mysqli) && (!$mysqli->connect_errno))
+	        {
+	        	// Write watervalues to database
+                        $mysqli->query("INSERT INTO `watermeter` (m3) VALUES (".$msg.");");
+
+                        // Read values from database
+                        if ($result = $mysqli->query("SELECT * FROM `watermeter` WHERE timestamp >= CURDATE() ORDER BY timestamp ASC LIMIT 1"))
+                        {
+                                $row = $result->fetch_object();
+                                //var_dump ($row);
+                                $newdata["today"]["m3"] = number_format($newdata["total"]["m3"] - $row->m3,3);
+                                $mqtt->publish($topicprefix."m3_today", $newdata["today"]["m3"], 0,1);
+                        }
+                        else
+                        {
+                                echo "error reading water values from database ".$mysqli->error."\n";
+                        }
+
+
+
+                        // Read values from database
+                        if ($result = $mysqli->query("SELECT * FROM `watermeter` WHERE DATE(timestamp) = CURDATE() - INTERVAL 1 DAY ORDER BY timestamp ASC LIMIT 1"))
+                        {
+                                $row = $result->fetch_object();
+                                if ($row)
+                                {
+                                        $newdata['yesterday']['m3'] = round($newdata["total"]["m3"] - $row->m3 - ["today"]["m3"],3);
+                                        $mqtt->publish($topicprefix."m3_yesterday", $newdata["yesterday"]["m3"],0,1);
+                                }
+                                else $mqtt->publish($topicprefix."m3_yesterday", "",0,1);   
+                        }
+                        else
+                        {
+                                echo "error reading water values from database ".$mysqli->error."\n";
+                        }
+
+
+                        // Read values from database
+                        if ($result = $mysqli->query("SELECT * FROM `watermeter` WHERE DATE(timestamp) = CURDATE() - INTERVAL 1 MONTH ORDER BY timestamp ASC LIMIT 1"))
+                        {
+                                $row = $result->fetch_object();
+                                $newdata['month']['m3'] = round($newdata["total"]["m3"]  - $row->m3, 3);
+                                $mqtt->publish($topicprefix."m3_month", $newdata["month"]["m3"]);
+                        }
+                        else
+                        {
+                                echo "error reading water values from database ".$mysqli->error."\n";
+                        }
+
+                        // Read values from database
+                        if ($result = $mysqli->query("SELECT * FROM `watermeter` WHERE timestamp >= DATE_FORMAT(NOW() ,'%Y-01-01') ORDER BY timestamp ASC LIMIT 1"))
+                        {
+                                $row = $result->fetch_object();
+                                var_dump ($row);
+                                $newdata['year']['m3'] = round($newdata["total"]["m3"]  - $row->m3, 3);
+                                $mqtt->publish($topicprefix."m3_year", $newdata["year"]["m3"]);
+                        }
+                        else
+                        {
+                                echo "error reading water values from database ".$mysqli->error."\n";
+                        }
+	        
+			$mysqli->close();
+	        }
+		else
+		{
+			echo "Connection to myqsl failed!\n";
+		}
+		
+		
+
+		
+	
+}
